b3f739dc4b7e1ab2fb454f969785853cfd17fc93
gugaobai <18721182863@163.com>
2017-04-23 10:33:05 +0800

    add rtc

--- example/rtc.js
+++ example/rtc.js
@@ -2,19 +2,47 @@
 
 var Pili = require('../index');
 
-var ACCESS_KEY = 'ak';
-var SECRET_KEY = 'sk';
-
-var HUB = '1212';
+var ACCESS_KEY = 'DWQOcImtTCrnPp1ogwgAHBdIK1mIFrnmtnXb-66-';
+var SECRET_KEY = 'cJFhYuaq7Vo35e1XDFUG8Rm8C2VjkpmXO0aGkJGM';
 
 var roomName = 'test1';
 var ownerId = 'idfive';
-var userMax = '6';
+var userMax = 6;
 
 var credentials = new Pili.Credentials(ACCESS_KEY, SECRET_KEY);
 var rtc = new Pili.RTC(credentials);
 
 rtc.createRoom(ownerId, roomName, userMax, function(err, roomInfo) {
-
+    if (!err) {
+        console.log(roomInfo);
+    } else {
+        console.log(err)
+    }
 });
 
+rtc.getRoom(roomName, function(err, roomInfo) {
+    if (!err) {
+        console.log(roomInfo);
+    } else {
+        console.log(err)
+    }
+});
+
+rtc.deleteRoom(roomName, function(err, roomInfo) {
+    if (!err) {
+        console.log(roomInfo);
+    } else {
+        console.log(err)
+    }
+});
+
+var roomAccess = {
+    "room_name" : "123",
+    "user_id" : "456",
+    "perm" : "admin",
+    "expire_at" : 3600
+}
+
+var roomToken = rtc.roomToken(roomAccess);
+
+console.log('roomToken:',roomToken)
