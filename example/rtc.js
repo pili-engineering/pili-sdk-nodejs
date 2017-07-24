3e8d205e20fb8f81ec4be2a70d30956ad7d700b7
guhao <guhao@qiniu.com>
2017-07-24 13:46:20 +0800

    rtc

--- example/rtc.js
+++ example/rtc.js
@@ -7,8 +7,8 @@ var Pili = require('../index.js');
 
 // ======================== Configurations =========================
 // Replace with your keys here
-var ACCESS_KEY = 'FMVCRs2-LO1ivRNi4l7mEZE6ZDvPv-519D12kZCO';
-var SECRET_KEY = 'InOXBls8alaPiRcFn002XsoXKFw1iFJZxcoOvAeY';
+var ACCESS_KEY = 'FMVCRs2-LO1ivNi4l7mEZE6ZDvPv-519D12kZCO';
+var SECRET_KEY = 'InOXBls8alaPicFn002XsoXKFw1iFJZxcoOvAeY';
 
 // Replace with your hub name
 var HUB = 'qiniu-pili-test'; // The Hub must be exists before use
@@ -20,49 +20,37 @@ var HUB = 'qiniu-pili-test'; // The Hub must be exists before use
 var credentials = new Pili.Credentials(ACCESS_KEY, SECRET_KEY);
 var rtc = new Pili.RTC(credentials);
 
-// var roomName = 'roomName2';
-// var ownerID = 'ownerID';
-// rtc.createRoom(ownerID, roomName, {}, function(err, roomInfo) {
-//     if (!err) {
-//         console.log(roomInfo);
-//         rtc.getRoom(roomInfo.room_name, function(err, info) {
-//             if (!err) {
-//                 console.log(info);
-//                 rtc.deleteRoom(roomInfo.room_name, function(err) {
-//                     if (!err) {
-//                         console.log("delete ok");
-//                     } else {
-//                         console.log(err);
-//                     }
-//                 })
-//             } else {
-//                 console.log(err);
-//             }
-//         })
-//     } else {
-//         console.log(err);
-//         rtc.deleteRoom(roomName, function(err) {});
-//     }
-// })
-//
-// var roomToken = rtc.roomToken({
-//     "room_name": roomName,
-//     "user_id": ownerID,
-//     "perm": "user"
-// })
-// console.log(roomToken);
+var roomName = 'roomName2';
+var ownerID = 'ownerID';
+rtc.createRoom(ownerID, roomName, {}, function(err, roomInfo) {
+    if (!err) {
+        console.log(roomInfo);
+        rtc.getRoom(roomInfo.room_name, function(err, info) {
+            if (!err) {
+                console.log(info);
+                rtc.deleteRoom(roomInfo.room_name, function(err) {
+                    if (!err) {
+                        console.log("delete ok");
+                    } else {
+                        console.log(err);
+                    }
+                })
+            } else {
+                console.log(err);
+            }
+        })
+    } else {
+        console.log(err);
+        rtc.deleteRoom(roomName, function(err) {});
+    }
+})
 
-// rtc.createRoom('112233', 'room111', {}, function(err, roomInfo) {
-// });
+var roomToken = rtc.roomToken({
+    "room_name": roomName,
+    "user_id": ownerID,
+    "perm": "user"
+})
+console.log(roomToken);
 
-// rtc.getRoom('room111',  function(err, info) {
-//    console.log(info);
-// });
 
-rtc.getRoomMember('room111', function (err, users) {
-    console.log(users);
-});
 
-// rtc.deleteRoomMember('room111', '112233', function (err, res) {
-//     console.log(res);
-// });
